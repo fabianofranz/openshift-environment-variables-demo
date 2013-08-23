a2556dad1cfdd91f15c3c8a59ef01a79b3ee06d0
fabianofranz <contact@fabianofranz.com>
2013-08-23 12:57:35 -0300

    foo

--- application.rb
+++ application.rb
@@ -4,34 +4,33 @@ require 'pony'
 
 get '/' do
   "Hey there!
-    <br /><br />It's a good day to <a href=\"/send\">send some emails</a>."
+   <br />
+   It's a good day to <a href=\"/send\">send some emails</a>."
 end
 
 get '/send' do
-  begin
-  Pony.mail({
-    :to => 'ffranz@redhat.com',
-    :from => ENV['SMTP_USERNAME'],
-    :subject => "From Awesomeness Demo #{Time.now}",
-    :body => 'Hey there',
-    :via => :smtp,
-    :via_options => {
-      :address        => ENV['SMTP_HOST'],
-      :port           => ENV['SMTP_PORT'],
-      :user_name      => ENV['SMTP_USERNAME'],
-      :password       => ENV['SMTP_PASSWORD'],
-      :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
-      :domain         => "localhost.localdomain", # the HELO domain provided by the client to the server
-      :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,
-      :enable_starttls_auto => true
-    }
-  })
-  "Sent! #{ENV['SMTP_HOST']}"
-  rescue Exception => e
-    return "Exception #{e.message} #{e.backtrace.to_s}"
+  if 'PRODUCTION' == ENV['ENVIRONMENT']
+    to = 'ffranz@redhat.com'
+    Pony.mail({
+      :to => to,
+      :from => ENV['SMTP_USERNAME'],
+      :subject => "From Awesomeness Demo #{Time.now}",
+      :body => 'Hey there',
+      :via => :smtp,
+      :via_options => {
+        :address        => ENV['SMTP_HOST'],
+        :port           => ENV['SMTP_PORT'],
+        :user_name      => ENV['SMTP_USERNAME'],
+        :password       => ENV['SMTP_PASSWORD'],
+        :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
+        :domain         => "localhost.localdomain", # the HELO domain provided by the client to the server
+        :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,
+        :enable_starttls_auto => true
+      }
+    })
+    "Sent (really) to #{to}!"
+  else
+    "Sent (kind of)!"
   end
 end
 
-get '/agent' do
-  "you're using #{request.user_agent}"
-end
