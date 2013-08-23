d7c132f8a0f980f0e8c408e6e88cd0a1c007ce9d
fabianofranz <contact@fabianofranz.com>
2013-08-23 12:24:39 -0300

    Hopefully sending emails

--- application.rb
+++ application.rb
@@ -2,8 +2,25 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  "the time where this server lives is #{Time.now}
-    <br /><br />check out your <a href=\"/agent\">user_agent</a>"
+  "Hey there.
+    <br /><br />Wny not to <a href=\"/mail\">send some emails</a>?"
+end
+
+get '/mailer' do
+  Pony.mail({
+    :to => 'ffranz@redhat.com',
+    :subject => "From Awesomeness Demo #{Time.now}",
+    :body => 'Hey there',
+    :via => :smtp,
+    :via_options => {
+      :address        => ENV['SMTP_HOST'],
+      :port           => ENV['SMTP_PORT'],
+      :user_name      => ENV['SMTP_USERNAME'],
+      :password       => ENV['SMTP_PASSWORD'],
+      :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
+      :domain         => "localhost.localdomain" # the HELO domain provided by the client to the server
+    }
+  })
 end
 
 get '/agent' do
